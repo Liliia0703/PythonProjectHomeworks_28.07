485088dc91cf6e2ffe2800913cf0b8e4fa189e98
Liliia0703 <Lil9I7733@gmail.com>
2025-08-11 15:49:40 +0300

    Виконані зміни

---
+++ lesson_02/homework_02.py
@@ -0,0 +1,103 @@
+#task1
+print("Task_1")
+print("Hello", end = " ")
+print("World")
+print()
+
+#task2
+print("Task_2")
+hello = "Hello"
+world = "world"
+if True: print(f"{hello} {world}!")
+print()
+
+#task3
+print("Task_3")
+for letter in "Hello world!": print(letter)
+print()
+
+#task4
+print("Task_4")
+apples=2
+bananas_times_more = 4
+banana=apples * bananas_times_more
+print(f"{apples}")
+print(f"{banana}")
+print()
+
+#task5
+print("Task_5")
+storona_1 = 1
+storona_2 = 2
+storona_3 = 3
+storona_4 = 4
+print()
+
+#task6
+print("Task_6")
+perimetery = storona_1 + storona_2 + storona_3 + storona_4
+print(f"{perimetery}")
+print()
+
+#task7
+print("Task_7")
+apple_trees = 4
+pear_trees_more = 5
+pear_trees = apple_trees + pear_trees_more
+plum_trees_less = 2
+plum_trees = apple_trees - plum_trees_less
+together_trees = apple_trees + pear_trees + plum_trees
+print(f"Яблунь посадили {apple_trees}")
+print(f"Груш посадили на {pear_trees_more} більше, ніж яблунь. Відповідь: {pear_trees}")
+print(f"Слив посадили на {plum_trees_less} менше, ніж яблунь. Відповідь: {plum_trees}")
+print(f"Скільки всього дерев посадили в саду? Відповідь: {together_trees} дерева")
+print()
+
+#task8
+print("Task_8")
+temperature_morning = 5
+temperature_after_lunch_less = 10
+temperature_after_lunch = temperature_morning - temperature_after_lunch_less
+temperature_evening_more = 4
+temperature_evening = temperature_after_lunch + temperature_evening_more
+print(f"До обіду температура була {temperature_morning} градусів.")
+print(f"Після обіду температура опустилася на {temperature_after_lunch_less} градусів. Відповідь: {temperature_after_lunch} градусів.")
+print(f"Надвечір потепліло на {temperature_evening_more} градуси. Яка температура надвечір? Відповідь: {temperature_evening} градус.")
+print()
+
+#task9
+print("Task_9")
+boys = 24
+girls_times_less = 2
+girls = boys // girls_times_less
+sick_boy = 1
+boys_left = boys - sick_boy
+absent_girls = 2
+girls_left = girls - absent_girls
+together = boys_left + girls_left
+print(f"У театральному гуртку {boys} хлопчики")
+print(f"Дівчаток у {girls_times_less} рази менше за хлопчиків. Відповідь: {girls}")
+print(f"{sick_boy} хлопчик захворів. Відповідь: {boys_left}")
+print(f"А {absent_girls} дівчинки не прийшли сьогодні. Відповідь: {girls_left}")
+print(f"Скільки дітей у театральному гуртку сьогодні? Відповідь: {together}")
+print()
+
+#task10
+print("Task_10")
+first_book = 8
+second_book_more = 2
+second_book = first_book + second_book_more
+together_first_second = first_book + second_book
+third_book_half_price = 2
+third_book = together_first_second // third_book_half_price
+together_books = first_book + second_book + third_book
+print(f"Перша книга коштує {first_book} грн")
+print(f"Друга книга на {second_book_more} грн дорожча. Відповідь: {second_book} грн")
+print(f"А третя - як половина вартості першої та другої разом. Відповідь: {together_first_second} грн. Вартість третьої книги: {third_book} грн")
+print(f"Скільки будуть коштувати книги якщо купити по одному примірнику разом? Відповідь: {together_books} грн")
+
+
+
+
+
+
