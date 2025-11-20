d843c8aa0530c8cc0c3696182feb172e527a2198
Liliia0703 <Lil9I7733@gmail.com>
2025-11-20 14:56:18 +0200

    Виконані зміни

---
+++ lesson_16/homework_16.1.py
@@ -0,0 +1,33 @@
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+
+class Manager(Employee):
+    def __init__(self, name, salary, department):
+        super().__init__(name, salary)
+        self.department = department
+
+
+class Developer(Employee):
+    def __init__(self, name, salary, programming_language):
+        super().__init__(name, salary)
+        self.programming_language = programming_language
+
+
+class TeamLead(Manager, Developer):
+    def __init__(self, name, salary, department, programming_language, team_size):
+        Employee.__init__(self, name, salary)  # викликаємо вручну, щоб не було конфлікту MRO
+        self.department = department
+        self.programming_language = programming_language
+        self.team_size = team_size
+
+
+lead = TeamLead("Лілія", 5000, "Development", "Python", 10)
+
+print(hasattr(lead, "department"))
+print(hasattr(lead, "programming_language"))
+print(hasattr(lead, "team_size"))
+print(hasattr(lead, "name"))
+print(hasattr(lead, "salary"))
